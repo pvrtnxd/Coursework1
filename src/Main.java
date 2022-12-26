02bbcfcef8bb0aa8815c9b935aa30e9cd611b817
lebed <lebedamm@gmail.com>
2022-12-26 15:40:08 +0300

    coursework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.nio.charset.CoderResult;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Coursework.printEmployee();
+        Coursework.printFullNames();
+        System.out.println(Coursework.calcAverageSalary());
+        System.out.println(Coursework.calcTotalSalary());
+        System.out.println(Coursework.findEmployeeWithMinSalary());
+        System.out.println(Coursework.findEmployeeWithMaxSalary());
+
+
+
+
     }
+
+
+
+
+
 }
